3a271fc56d47608059f1ebfe87ef5a7deb3f2128
Divine "KamCy" Ahaka <69519905+KamcyAhaka@users.noreply.github.com>
2026-03-23 20:20:16 +0100

    feat: add global Nuxt middleware to remove trailing slashes from routes

---
+++ app/middleware/trailing-slash.global.ts
@@ -0,0 +1,8 @@
+export default defineNuxtRouteMiddleware((to) => {
+  if (to.path !== "/" && to.path.endsWith("/")) {
+    const { path, query, hash } = to;
+    const nextPath = path.replace(/\/+$/, "") || "/";
+
+    return navigateTo({ path: nextPath, query, hash }, { redirectCode: 301 });
+  }
+});
